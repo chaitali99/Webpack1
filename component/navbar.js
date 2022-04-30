88c8706a48b613f59cd18e575633e160f91904e5
chaitali amale <chaitaliamale09@gmail.com>
2022-04-30 23:18:11 +0530

    Webpack Assignment1

---
+++ component/navbar.js
@@ -0,0 +1,22 @@
+function navbar(){
+    return `<h3>Home</h3>
+    
+    <div id="option">
+    <h3>
+        <a href="jewelery.html">Jewelery</a>
+    </h3>
+    <h3>
+        <a href="electronic.html">Electronic</a>
+    </h3>
+
+    <h3>
+        <a href="login.html">Log In</a>
+    </h3>
+    <h3>
+        <a href="signup.html">Sign Up</a>
+    </h3>
+
+        </div>`;
+}
+
+export  { navbar };
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {navbar} from "../component/navbar";
+import '../styles/navbar.css';
+// console.log('Navbar:',navbar)
+// document.getElementById("body").innerHTML=navbar();
+let navbar_div=document.getElementById("navbar");
+
+navbar_div.innerHTML=navbar();
